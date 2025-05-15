28c4548cd8ae2e3f42bb31c66b9ea42ff2d1aa73
romOlivo <al341918@uji.es>
2025-05-15 19:49:03 +0200

    Tested replace_line_and_write_output and fixed

--- translation_tests/test_scenes_adapters.py
+++ translation_tests/test_scenes_adapters.py
@@ -18,5 +18,25 @@ class TestScenesAdapter:
         scenes_adapters.set_global_path("hey/")
         assert scenes_adapters._get_path_rpy(scene_name, path_to_scene) == expected_path
 
+    @pytest.mark.parametrize("line,expected_result", [
+        ('some visual command', 'some visual command\n'),
+        ('"hello"', '"[my_var[0]]"\n'),
+        ('"hello [something]"', '"[my_var[0]][something][my_var[1]]"\n'),
+        ('"[something] bye"', '"[my_var[0]][something][my_var[1]]"\n'),
+        ('"hello [something] bye"', '"[my_var[0]][something][my_var[1]]"\n'),
+        ('command "hello"', 'command "[my_var[0]]"\n'),
+        ('"[ellipse]"', '"[ellipse]"\n'),
+        ('"hello [ellipse]"', '"[my_var[0]][ellipse][my_var[1]]"\n'),
+        ('"[ellipse] bye"', '"[my_var[0]][ellipse][my_var[1]]"\n'),
+        ('"hello [ellipse] bye"', '"[my_var[0]][ellipse][my_var[1]]"\n'),
+        ('command1 command2 command3 "some text"', 'command1 command2 command3 "[my_var[0]]"\n'),
+        ('"one [first] two [second] three [third] end"', '"[my_var[0]][first][my_var[1]][second][my_var[2]][third][my_var[3]]"\n'),
+    ])
+    def test_replace_line_and_write_output(self, line, expected_result):
+        var_name = 'my_var'
+        scenes_adapters.pos_var = 0
+        assert scenes_adapters._replace_line_and_write_output(line, var_name) == expected_result
+
+
 
 
