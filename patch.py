024bfb2cf44567d133ea4c02b002cb946f11d87a
romOlivo <al341918@uji.es>
2025-05-16 13:49:31 +0200

    Patch for adapting day 1 done

--- patch.py
+++ patch.py
@@ -1,6 +1,10 @@
 from translation_utils import convert_scene, set_global_path
 
 if __name__ == "__main__":
-    set_global_path("/testing_area")
-    convert_scene("prologue", "/day1")
+    set_global_path("/scenes/")
+    with open("translations/config.csv") as f:
+        config = f.read().split("\n")[1:]
+        for scene_config in config:
+            scene_config_split = scene_config.split("#")
+            convert_scene(scene_config_split[0], scene_config_split[1])
 
