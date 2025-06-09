584ac793d1c42952fabc87ad51f6e59686f34ffd
romOlivo <al341918@uji.es>
2025-06-09 20:05:01 +0200

    Fixed again

--- patch.py
+++ patch.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
             scene_config_split = scene_config.split("#")
             convert_scene(scene_config_split[0], scene_config_split[1], write_out=True)
     adapt_scene()
-    shutil.rmtree("./testing_area/translation_tests")
+    shutil.rmtree("./translation_tests")
 
